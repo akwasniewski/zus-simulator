d7244ffbb1692d43d3395a7efcce0950f6269e4e
Michał Bawołek <michal.bawolek@gmail.com>
2025-10-04 17:34:51 +0200

    Bugfix

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,10 +34,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <Header />
       <body
         className={`${geistSans.variable} ${geistMono.variable} ${lato.variable} antialiased`}
       >
+      <Header />
         {children}
       
       <FunFactsCarousel />
